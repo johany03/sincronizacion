e55be2d74fd3f5c3dfd39b12b04ceb156da5fa3f
developerJohany <82184229+developerJohany@users.noreply.github.com>
2021-04-08 17:31:53 -0500

    Update nav.php

---
+++ nav.php
@@ -0,0 +1,15 @@
+<header>
+
+  <img src="/assets/images/falling-star-logo.png">
+
+  <h1>La estrella fugaz</h1>
+
+  <form action="search.php">
+
+    <input type="text">
+
+    <input type="submit" value="Buscar">
+
+  </form>
+
+</header>
